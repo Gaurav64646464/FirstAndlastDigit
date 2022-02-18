67d1112c3b2d1465d7e63e857aced41dffc85a3f
admin <gaurav.sarodkar@perennialsys.com>
2022-02-18 13:32:15 +0530

    Merge remote-tracking branch 'origin/origin/main' into origin/main

--- JavaExecutionProjects/src/com/company/Main.java
+++ JavaExecutionProjects/src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 import com.company.EvenDigitSum;
+
+import static com.company.LastDigitChecker.hasSameLastDigit;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,5 +10,8 @@ public class Main {
     evenDigitSum.getEvenDigitSum(8);
     SharedDigits sharedDigits=new SharedDigits();
         System.out.println(sharedDigits.shareDigit(5,6));
+
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(231, 372, 423));
     }
 }
