c8841eb6e597d152788229ca519af643111e9efe
admin <gaurav.sarodkar@perennialsys.com>
2022-02-18 13:36:18 +0530

    Merge remote-tracking branch 'origin/origin/main' into origin/main

---
+++ Classes_Constructor_Inheritance/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SumCalculator Sc=new SumCalculator(56,78);
+        System.out.println(Sc.getAdditionResult()+"  =Addition  "+Sc.getdivisionResult()+"  =division  "+Sc.getMultiplicationResult()+"  =Multiplication  "+Sc.getSubstractionResult()+"  Subtracted  ");
+    }
+}
