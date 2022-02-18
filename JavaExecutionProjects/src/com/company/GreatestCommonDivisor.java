6c5bdb7ca8aaf248f939725ee0e9219acc15efc9
admin <gaurav.sarodkar@perennialsys.com>
2022-02-18 13:36:18 +0530

    Merge remote-tracking branch 'origin/origin/main' into origin/main

---
+++ JavaExecutionProjects/src/com/company/GreatestCommonDivisor.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int n1, int n2) {
+        int gcd = 1;
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+}
